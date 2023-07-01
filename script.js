bb6de008f2ee60f131af2747d18caa6a4725c6bf
TheIanAnderson <131201726+TheIanAnderson@users.noreply.github.com>
2023-07-01 18:11:08 -0500

    Added new functions in js to populate information retrieved from API, as well as new HTML elements

--- script.js
+++ script.js
@@ -1,28 +1,30 @@
-const apiKey = "bc84d055ec0f2dab044ce4d973a5b1d4";
+const API_KEY = "bc84d055ec0f2dab044ce4d973a5b1d4";
 const apiURL = "https://api.openweathermap.org/";
 const searchButton = document.getElementById("myButton");
 const searchInput = document.querySelector(".form-control");
 const displayDate = document.getElementById("todays-date");
-const todaysWeather = document.getElementsByClassName("results");
+const makeResultsVisible = document.querySelector("#results");
+const todaysWeather = document.querySelector("#result1");
 const userInput = searchInput.value;
 const todaysDate = dayjs().format("dddd, MMMM YYYY");
 const currentTime = dayjs().format("h:mm A");
-
+console.log(makeResultsVisible);
 displayDate.innerHTML = todaysDate;
-console.log(todaysWeather);
 
 searchButton.addEventListener("click", function () {
   cityLocationFetch(searchInput.value);
+  makeResultsVisible.setAttribute("style", "display:flex");
 });
 
 function cityLocationFetch(userInput) {
-  const getCityLocation = `https://api.openweathermap.org/geo/1.0/direct?q=${userInput}&limit=5&appid=${apiKey}`;
+  const getCityLocation = `https://api.openweathermap.org/geo/1.0/direct?q=${userInput}&limit=5&appid=${API_KEY}`;
   fetch(getCityLocation)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
       if (!data[0]) {
+        makeResultsVisible.setAttribute("style", "display:none");
         alert("Sorry, can't find that place.");
       } else {
         search = data[0];
@@ -32,6 +34,7 @@ function cityLocationFetch(userInput) {
         // fetchWeather(data[0][1][2]);
         setCityLon(cityLon);
         setCityLat(cityLat);
+        setCityName(cityName);
         currentWeatherFetch();
       }
     })
@@ -48,10 +51,14 @@ function setCityLat(variable) {
   localStorage.setItem("City Lat", variable);
 }
 
+function setCityName(variable) {
+  localStorage.setItem("City Name", variable);
+}
+
 function currentWeatherFetch() {
   var cityLat = localStorage.getItem("City Lat");
   var cityLon = localStorage.getItem("City Lon");
-  const getCurrentWeather = `https://api.openweathermap.org/data/2.5/weather?lat=${cityLat}&lon=${cityLon}&appid=${apiKey}&units=imperial`;
+  const getCurrentWeather = `https://api.openweathermap.org/data/2.5/weather?lat=${cityLat}&lon=${cityLon}&appid=${API_KEY}&units=imperial`;
   fetch(getCurrentWeather)
     .then(function (response) {
       return response.json();
@@ -60,8 +67,9 @@ function currentWeatherFetch() {
       if (data === "") {
         alert("Sorry, can't find that place.");
       } else {
-        console.log(data.main.temp);
         const cityTemp = data.main.temp;
+        // console.log(cityTemp)
+        displayWeather(cityTemp);
       }
     })
     .catch(function (error) {
@@ -69,4 +77,8 @@ function currentWeatherFetch() {
     });
 }
 
-function displayWeather() {}
+function displayWeather(data) {
+  const cityName = localStorage.getItem("City Name");
+  todaysWeather.innerHTML =
+    "The weather in " + cityName + " right now is " + data + " degrees!";
+}
